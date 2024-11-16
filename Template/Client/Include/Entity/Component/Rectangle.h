9872de859d8c6f606e8369d7e1972211b02d6beb
5900w <chaewan2002@gmail.com>
2024-11-16 23:47:09 +0900

    added Rectangle Component

---
+++ Template/Client/Include/Entity/Component/Rectangle.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Component.h"
+
+class CRectangle : public CComponent
+{
+public:
+	CRectangle() = default;
+	virtual ~CRectangle();
+
+public:
+    virtual bool Init();
+    virtual void Update(float DeltaTime);
+    virtual void Render(SDL_Renderer* Renderer);
+
+private:
+	virtual bool Release() final;	// Object.h에서 component memoryPool usage 위하여
+};
