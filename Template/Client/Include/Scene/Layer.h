a19c5e5b58aa842c1d2e43c0cd0a0e68d39a2d55
Woo95 <chaewan2002@gmail.com>
2025-01-29 08:49:32 +0900

    added layer class for scene

---
+++ Template/Client/Include/Scene/Layer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../Core/GameInfo.h"
+#include "../Core/LayerUtils.h"
+
+class CLayer
+{
+	friend class CScene;
+
+public:
+	CLayer();
+	~CLayer();
+
+private:
+    std::vector<class CObject*> mObjects;
+
+	ESort::Type mSort = ESort::Y;
+
+protected:
+	void Update(float DeltaTime);
+	void LateUpdate(float DeltaTime);
+	void Render(SDL_Renderer* Renderer);
+
+public:
+	const std::vector<CObject*>& GetObjectVec() { return mObjects; }
+	void SetSortType(ESort::Type type) { mSort = type; }
+
+	void AddObject(CObject* obj)
+	{
+		mObjects.emplace_back(obj);
+	}
+
+private:
+	static bool SortY(CObject* a, CObject* b);
+};
