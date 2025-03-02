364ee3c662a6b7ff3b4a10bbfd9f9357919b51e6
Woo95 <chaewan2002@gmail.com>
2025-03-03 01:32:55 +0900

    added AnimatedImage Widget

---
+++ Template/Client/Include/Widget/AnimatedImage.cpp
@@ -0,0 +1,40 @@
+#include "AnimatedImage.h"
+#include "../Manager/Resource/AssetManager.h"
+#include "../Manager/Resource/TextureManager.h"
+#include "../Manager/Resource/UIManager.h"
+#include "../Resource/Texture.h"
+
+CAnimatedImage::CAnimatedImage()
+{
+}
+
+CAnimatedImage::~CAnimatedImage()
+{
+}
+
+void CAnimatedImage::Render(SDL_Renderer* Renderer)
+{
+	CWidget::Render(Renderer);
+
+	SDL_SetTextureAlphaMod(mTexture.get()->GetTexture(), mAlpha);
+
+	SDL_RenderCopy(Renderer, mTexture.get()->GetTexture(), &mFrames[mCurrIdx], &mRect);
+}
+
+void CAnimatedImage::Update(float DeltaTime)
+{
+	CWidget::Update(DeltaTime);
+
+	mFrameInterval += DeltaTime;
+
+	if (mFrameInterval >= mIntervalPerFrame)
+	{
+		if (mIsLoop)
+			mCurrIdx = (mCurrIdx + 1) % mFrames.size();
+		else if (mCurrIdx < mFrames.size() - 1)
+		{
+			mCurrIdx++;
+		}
+		mFrameInterval = 0.0f;
+	}
+}
---
+++ Template/Client/Include/Widget/AnimatedImage.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Image.h"
+
+class CAnimatedImage : public CImage
+{
+public:
+	CAnimatedImage();
+	virtual ~CAnimatedImage();
+
+private:
+	int mCurrIdx = 0;
+
+	bool mIsLoop = true;
+
+	float mFrameInterval = 0;
+	float mIntervalPerFrame = 0.3f;
+
+private:
+	virtual void Render(SDL_Renderer* Renderer);
+	virtual void Update(float DeltaTime);
+};
