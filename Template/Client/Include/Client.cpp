2fc9c5a88586435c0fa0ac27d344b9983de95231
Woo95 <chaewan2002@gmail.com>
2025-03-05 02:32:12 +0900

    added sdl font library (SDL_ttf)

--- Template/Client/Include/Client.cpp
+++ Template/Client/Include/Client.cpp
@@ -4,6 +4,7 @@
 #pragma comment(lib, "SDL2.lib")
 #pragma comment(lib, "SDL2main.lib")
 #pragma comment(lib, "SDL2_image.lib")
+#pragma comment(lib, "SDL2_ttf.lib")
 
 int main(int argc, char* argv[])
 {
